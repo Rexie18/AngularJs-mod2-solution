5a3253f9a3465bfaf9f96b5a87731ee4b202c4b2
Rexie18 <ro@students.kiron.ngo>
2017-03-24 22:16:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var shoppingList2 = [
   }
 ];
 
-angular.module('ShoppingListApp', [])
+angular.module('ShoppingListCheckOff', [])
 .controller('ToBuyController', ToBuyController)
 .controller('AlreadyBoughtController', AlreadyBoughtController)
 .service('ShoppingListService', ShoppingListService)
