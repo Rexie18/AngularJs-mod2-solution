36fb4fa6018abdd2db0d63399788de8566f9c723
Rexie18 <ro@students.kiron.ngo>
2017-03-24 19:14:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
+
 (function () {
 'use strict';
 
-angular.module('ShoppingListCheckOff', [])
-.controller('ToBuyController', ToBuyController)
-.controller('AlreadyBoughtController', AlreadyBoughtController)
-.service('ShoppingListCheckOffService', ShoppingListCheckOffService)
-
-ToBuyController.$inject = ['$scope'];
-AlreadyBoughtController.$inject = ['$scope'];
-  
-function ToBuyController($scope) {
-  var ToBuyController = [
+var ToBuyController = [
   {
     name: "Milk",
     quantity: "2"
@@ -28,19 +20,70 @@ function ToBuyController($scope) {
     quantity: "5"
   }
 ];
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController);
+
+ToBuyController.$inject = ['$scope'];
+function ToBuyController($scope) {
   $scope.ToBuyController = ToBuyController;
   
-  $scope.AlreadyBoughtController = function () {
+  $scope.addToList = function () {
     var newItem = {
       name: $scope.newItemName,
       quantity: $scope.newItemQuantity
     };
 
-    $scope.AlreadyBoughtController.push(newItem);
+    $scope.ToBuyController.push(newItem);
   };
 }
 
 })();
 
 
-//http://jsfiddle.net/ehmorzpx/
+// (function () {
+// 'use strict';
+
+// angular.module('ShoppingListCheckOff', [])
+// .controller('ToBuyController', ToBuyController)
+// .controller('AlreadyBoughtController', AlreadyBoughtController)
+// .service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+
+// ToBuyController.$inject = ['$scope'];
+// AlreadyBoughtController.$inject = ['$scope'];
+  
+// function ToBuyController($scope) {
+//   var ToBuyController = [
+//   {
+//     name: "Milk",
+//     quantity: "2"
+//   },
+//   {
+//     name: "Donuts",
+//     quantity: "200"
+//   },
+//   {
+//     name: "Cookies",
+//     quantity: "300"
+//   },
+//   {
+//     name: "Chocolate",
+//     quantity: "5"
+//   }
+// ];
+//   $scope.ToBuyController = ToBuyController;
+  
+//   $scope.AlreadyBoughtController = function () {
+//     var newItem = {
+//       name: $scope.newItemName,
+//       quantity: $scope.newItemQuantity
+//     };
+
+//     $scope.AlreadyBoughtController.push(newItem);
+//   };
+// }
+
+// })();
+
+
+// //http://jsfiddle.net/ehmorzpx/
