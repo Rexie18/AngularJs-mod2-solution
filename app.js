8874a9101d2e2ad6ce0963806393246e66a266e2
Rexie18 <ro@students.kiron.ngo>
2017-03-29 15:32:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,6 @@
 (function () {
 'use strict';
-  
-angular.module('ShoppingListCheckOff', [])
-.controller('ToBuyController', ToBuyController)
-.controller('AlreadyBoughtController', AlreadyBoughtController)
-.service('ShoppingListService', ShoppingListService);
-
-ToBuyController.$inject = ['ShoppingListService'];
-function ToBuyController(ShoppingListService) {
-  var itemAdder = this;
- itemAdder.shoppingList2 = shoppingList2;
-}
-
-AlreadyBoughtController.$inject = ['ShoppingListService'];
-function AlreadyBoughtController(ShoppingListService) {
- var boughtItem= this;
-
- boughtItem.items = ShoppingListService.getItems();  
-}
-
-function ShoppingListService( ) {
-  var service = this;
- var shoppingList2 = [
+  var shoppingList2 = [
   {
     name: "Milk",
     quantity: "2"
@@ -43,6 +22,28 @@ function ShoppingListService( ) {
     quantity: "5"
   }
 ];
+  
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListService', ShoppingListService);
+
+ToBuyController.$inject = ['ShoppingListService'];
+function ToBuyController(ShoppingListService) {
+  var itemAdder = this;
+ itemAdder.shoppingList2 = shoppingList2;
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListService'];
+function AlreadyBoughtController(ShoppingListService) {
+ var boughtItem= this;
+
+ boughtItem.items = ShoppingListService.getItems();  
+}
+
+function ShoppingListService( ) {
+  var service = this;
+ 
     service.move = function() {
       service.boughtItem.push(service.shoppingList2.splice(temIdex, 1));
     };
