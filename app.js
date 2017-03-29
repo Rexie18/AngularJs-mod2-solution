40210cb013351a67874b3e6befec6e1e88163d54
Rexie18 <ro@students.kiron.ngo>
2017-03-29 20:40:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ AlreadyBoughtController.$inject = ['ShoppingListService'];
 function AlreadyBoughtController(ShoppingListService) {
  var boughtItem= this;
 
- boughtItem.items = service.move();  
+ boughtItem.items = service.move;  
 }
 
 function ShoppingListService( ) {
