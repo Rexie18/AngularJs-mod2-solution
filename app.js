7f2fd40515a4f424fd3b76b19b9365350b1617ca
Rexie18 <ro@students.kiron.ngo>
2017-03-30 15:16:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ angular.module('ShoppingListCheckOff', [])
 ToBuyController.$inject = ['ShoppingListService'];
 function ToBuyController(ShoppingListService) {
   var itemAdder = this;
- itemAdder.shoppingList2 = shoppingList2;
+ itemAdder = shoppingList2;
 }
 
 AlreadyBoughtController.$inject = ['ShoppingListService'];
