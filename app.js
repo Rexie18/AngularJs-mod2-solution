06c81b9158de4528a749611b7567f5711436dad8
Rexie18 <ro@students.kiron.ngo>
2017-04-02 21:34:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ function ShoppingListService( ) {
  
 var bought = [];
 
-service.addData = function(remove) {   // function(remove)
+service.addData = function(itemAdder) {   // function(remove)
 	var item = {
 		"name": itemName,
 		"quantity": quantity
